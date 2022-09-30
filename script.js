db504c9a7d8dcce9313dab3e2cd8a3a14c07fb9d
djiby thioub <thioubdjiby96@gmail.com>
2022-09-30 23:33:26 +0000

    udate the navbar 30-09-2022

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-const scroll=()=>{
-    
+const hamburgerToggler = document.querySelector(".hamburger")
+const navLinksContainer= document.querySelector(".navlinks-container");
+const toggleNav = e =>{
+    hamburgerToggler.classList.toggle("open")
+    const ariaToggle=hamburgerToggler.getAttribute("aria-expanded")==="true"? "false":"true";
+    hamburgerToggler.setAttribute("ara-expanded", ariaToggle)
+    navLinksContainer.classList.toggle("open")
 }
+hamburgerToggler.addEventListener("click, toggleNav")
